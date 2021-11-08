75d19b8d4e228adfc28f325d02e7b0cd1e7683c3
Cyytrus <antoniopaulo122@gmail.com>
2021-11-08 15:37:55 -0300

    add helpers, routes, translations and edit some views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,14 @@ module ApplicationHelper
     def data_br(data_us)
         data_us.strftime("%d/%m/%Y")
     end
+
+    def ambiente_rails
+        if Rails.env.development?
+            "desenvolvimento"
+        elsif Rails.env.production?
+            "Produção"
+        else
+            "Testes"
+        end
+    end
 end
