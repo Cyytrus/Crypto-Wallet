f14aa1f10eca8e59f44b546988aebd3347202125
Cyytrus <antoniopaulo122@gmail.com>
2021-11-09 18:58:38 -0300

    add integrations of coins and mining types with db migrations, I18n gem and some translations

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,8 @@
 module ApplicationHelper
+    def locale(locale)
+        I18n.locale == :en ? "Inglês" : "Português do Brasil"
+    end
+    
     def data_br(data_us)
         data_us.strftime("%d/%m/%Y")
     end
