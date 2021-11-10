014f8e2681af3bdd04f68e77590d2cbf63253df7
Cyytrus <antoniopaulo122@gmail.com>
2021-11-10 15:18:04 -0300

    add locale cookies, javascript libs, I18n improvement and pipelines

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,6 @@
 class WelcomeController < ApplicationController
   def index
+    #cookies[:curso] = "Curso de Ruby On Rails"
+    session[:curso] = "Paulo Castrp"
   end
 end
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,17 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+
+Rails.application.config.assets.precompile += %w( cable.js
+                                                  coins.js
+                                                  jquery.js
+                                                  mining_types.js
+                                                  notify.js
+                                                  welcome.js
+                                                ) 
+
+Rails.application.config.assets.precompile += %w( coins.css
+                                                  mining_types.css
+                                                  scaffolds.css
+                                                  welcome.css
+                                                ) 
