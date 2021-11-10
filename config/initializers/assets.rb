207da9b9ea33cfddc2a20a55c310d23ab22464c0
Cyytrus <antoniopaulo122@gmail.com>
2021-11-10 18:09:20 -0300

    Add bootstrap, I18n translations, yarn libs and more

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -15,9 +15,7 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
 Rails.application.config.assets.precompile += %w( cable.js
                                                   coins.js
-                                                  jquery.js
                                                   mining_types.js
-                                                  notify.js
                                                   welcome.js
                                                 ) 
 
